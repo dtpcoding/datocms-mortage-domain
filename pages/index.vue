1c79e110d25b878173a768da203adf8cfd509cab
thanh <thanhp@mindarc.com.au>
2022-04-22 17:03:19 +1000

    back to ajax forms

--- pages/index.vue
+++ pages/index.vue
@@ -14,30 +14,48 @@
     </section>
 
     <section class="brokerage-form">
-     <form name="brokerage-form" method="POST" data-netlify="true" action="/#successfully-submitted">
-      <p>
-        <label>Your Name: </label>
-        <input type="text" name="name" />
-      </p>
-      <p>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </p>
-      <p>
-        <label>Your Role:</label>
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </p>
-      <p>
-        <label>Message: </label>
-        <textarea name="message"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+      <form method="post" @submit.prevent="handleSubmit">
+        <input type="hidden" name="form-name" value="contact-us-form" required />
+
+        <div class="form-row no-gutters">
+          <div class="form-group col-md-6">
+
+            <input type="text" class="form-control" id="firstname" name="firstname" placeholder="&nbsp;"
+              @input="ev => form.firstname = ev.target.value" required>
+            <label for="firstName">First name</label>
+          </div>
+          <div class="form-group col-md-6">
+
+            <input type="text" class="form-control" id="lastname" name="lastname" placeholder="&nbsp;"
+              @input="ev => form.lastname = ev.target.value" required>
+            <label for="lastName">Last name</label>
+          </div>
+        </div>
+        <div class="form-row no-gutters">
+          <div class="form-group col-md-12">
+
+            <input type="email" class="form-control" id="input-email" name="email" placeholder="&nbsp;"
+              @input="ev => form.email = ev.target.value" required>
+            <label for="inputEmail">Email</label>
+          </div>
+        </div>
+        <div class="form-row no-gutters">
+          <div class="form-group col-md-12">
+
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="&nbsp;"
+              @input="ev => form.subject = ev.target.value" required>
+            <label for="subject">Subject</label>
+          </div>
+        </div>
+        <div class="form-row no-gutters">
+          <div class="form-group col-md-12">
+            <textarea class="form-control" id="message" name="message" placeholder="&nbsp;"
+              @input="ev => form.message = ev.target.value" required></textarea>
+            <label for="message">Your message to us</label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit message</button>
+      </form>
     </section>
 
     <!-- <section class="hero">
@@ -241,13 +259,8 @@ export default {
           ...this.form
         })
       })
-        .then((response) => {
-          console.log(response);
-          console.log('form successfully submitted');
-        })
-        .catch(() => {
-          console.log('form unsuccessfully submitted');
-        });
+          .then(() => this.$router.push("/#form-submitted"))
+          .catch((error) => alert(error));
     }
   },
   head() {
